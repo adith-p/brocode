baccc12471074be68138e21318e6bee33eb7db96
adith-p <adithprakash008@gmail.com>
2023-07-05 20:22:58 +0530

    Commit: const keyword

---
+++ constCirle.js
@@ -0,0 +1,12 @@
+/* Js have a const keyword which make the variable Immutable using const gives you data security*/
+
+const PI = 3.14;
+
+let radius;
+let circumference;
+
+radius = Number(window.prompt("Enter the radius..."));
+
+circumference = 2 * PI * radius
+
+console.log(circumference)
