4e2476b3f064c315809abdcdb65c6f254eaad7cf
adith-p <adithprakash008@gmail.com>
2023-07-05 18:59:19 +0530

    Commit: arithmetic Operarions

---
+++ arithmetic.js
@@ -0,0 +1,22 @@
+/* arithmetic expressions is a combination of 
+Operands,
+Operators,
+Operands
+
+ex:
+    y = x + 9;
+*/
+
+
+let student = 20;
+
+student = student + 1; // addition
+student = student - 1; // subtraction
+student = student * 2; // Multiplication
+student = student / 2; // Division
+
+console.log(student)
+
+let remainingStudent = student % 3;// Moduls operator returns the reminder 
+
+console.log(remainingStudent);
